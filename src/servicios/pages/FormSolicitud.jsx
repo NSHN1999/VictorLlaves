cad0383796143e1b014bb8f732fca9223bb13d35
Nicolás Hernández <nico.hernandezekko@gmail.com>
2024-05-17 16:37:04 -0400

    Archivo HomePage modificado

---
+++ src/servicios/pages/FormSolicitud.jsx
@@ -0,0 +1,8 @@
+
+export const FormSolicitud = () => {
+    return (
+        <form>
+            <h1 className="text-center font-orbitron">SOLICITUD</h1>
+        </form>
+    );
+};
--- src/servicios/pages/HomePage.jsx
+++ src/servicios/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
     return (
@@ -13,6 +14,43 @@ export const HomePage = () => {
                 </p>
             </div>
             <h2 className="text-1xl text-center font-semibold text-red-700 font-orbitron">Servicio a domicilio</h2>
+            <div className="p-[10px] pb-[40px] mt-[40px] bg-white rounded-xl shadow-lg">
+                <h2 className="1xl text-center h-auto py-[15px] text-neutral-700">SERVICIOS</h2>
+                <div className="grid grid-cols-2 gap-4 gap-y-5 text-sm font-extralight">
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                    <button className="px-4 py-[2px] rounded-full bg-green-900 text-white">
+                        Cerrajeria
+                    </button>
+                </div>
+                <div className="flex justify-center content-center pt-[40px] font-extralight">
+                    <Link 
+                        className="px-4 py-[2px] bg-cyan-900 active:bg-cyan-700 text-white rounded-full"
+                        to="/solicitudes"    
+                    >
+                        Enviar solicitud
+                    </Link>
+                </div>
+            </div>
         </section>
         </>
     );
